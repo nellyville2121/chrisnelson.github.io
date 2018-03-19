1fce18f3d593b698e448e6a80e1a7989ea3a7c77
nellyville2121 <35713081+nellyville2121@users.noreply.github.com>
2018-03-18 21:29:07 -0600

    Add files via upload

---
+++ reddit_pseudo.js
@@ -0,0 +1,11 @@
+// Menu slides out when sidenav is clicked
+
+// Hide everything behind the menu when it slides out
+
+// When the user hovers over the menu items they change color
+
+// When the upvote arrow is clicked
+// score will go up
+
+// when the downvote is clicked
+// score will go down
